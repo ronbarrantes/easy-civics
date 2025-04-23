66ba7b07b4ac14684b3d030e16d1b2f1761b4092
Ron Barrantes <ronbarrantes@gmail.com>
2025-04-23 12:25:17 -0400

    Remove other files from the prev app

--- src/app/api/google/login/route.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-import { getGoogleAuthUrl } from "@/utils/google-oauth";
-
-export async function GET() {
-  const url = getGoogleAuthUrl();
-  redirect(url);
-}
