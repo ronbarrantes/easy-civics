fe2c18d7646fb60a99116d7d672730a6507cf55e
Ron Barrantes <ronbarrantes@gmail.com>
2025-04-23 15:27:20 -0400

    Another update

--- src/server/api/routers/qa-router.ts
+++ src/server/api/routers/qa-router.ts
@@ -1,9 +1,13 @@
-import { auth } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
-import { z } from "zod";
+// import { auth } from "@clerk/nextjs/server";
+// import { eq } from "drizzle-orm";
+// import { z } from "zod";
 
-import { decrypt, encrypt } from "@/lib/encryption";
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { tryCatch } from "@/utils/try-catch";
+// import { decrypt, encrypt } from "@/lib/encryption";
+import {
+  createTRPCRouter,
+  // publicProcedure
+  //
+} from "@/server/api/trpc";
+// import { tryCatch } from "@/utils/try-catch";
 
 export const qaRouter = createTRPCRouter({});
