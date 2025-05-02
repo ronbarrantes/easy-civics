525ca1bed3da8a84f92233522d36e75bc5546d8f
Ron Barrantes <ronbarrantes@gmail.com>
2025-05-02 17:19:45 -0400

    Add time util

---
+++ src/utils/time.ts
@@ -0,0 +1,6 @@
+// Format in seconds to mm:ss
+export const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+};
