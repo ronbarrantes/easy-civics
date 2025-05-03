1e91aa3e1b43311e598ef3ebec7eb3ff798aa049
Ron Barrantes <ronbarrantes@gmail.com>
2025-05-02 23:58:29 -0400

    Add js docs for the time

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,4 +1,8 @@
-// Format in seconds to mm:ss
+/**
+ * Formats a number of seconds into a string in the format "MM:SS".
+ * @param seconds - The number of seconds to format
+ * @returns
+ */
 export const formatTime = (seconds: number) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
