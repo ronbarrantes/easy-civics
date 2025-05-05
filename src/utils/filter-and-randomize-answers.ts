c1af59a1a6491110ed4b78f7cc1bff63c07bcc49
Ron Barrantes <ronbarrantes@gmail.com>
2025-05-05 11:48:33 -0400

    Optimize

--- src/utils/filter-and-randomize-answers.ts
+++ src/utils/filter-and-randomize-answers.ts
@@ -1,51 +1,40 @@
 import { QuestionWithAnswers } from "@/server/api/routers/qa-router";
-import { pickUniqueRandomNumbers } from "./random-numbers";
-import { randomizeArray } from "./randomize-array";
-
-const MAX_NUMBER_OF_ANSWERS = 5;
+import { MAX_NUMBER_OF_ANSWERS } from "@/utils/constants";
+import { randomizeArray } from "@/utils/randomize-array";
 
 const getQuestionReady = (
   question: QuestionWithAnswers
 ): QuestionWithAnswers => {
-  // TODO:I need to get the answers and the expected number of answers
-  // according to how many expected answers, I need to have at most that many correct
-  // answers and the rest being incorrect answers
-  // (maybe minus all the correct answers to add to no more than 5 answers total)
-  //
-  // filter the correct and incorrect answers
-  //
   const correctAnswers: QuestionWithAnswers["answers"] = [];
   const incorrectAnswers: QuestionWithAnswers["answers"] = [];
-  const answersToRandomize: QuestionWithAnswers["answers"] = [];
 
-  /// split the answers into correct and incorrect
+  // Split answers into correct and incorrect
   question.answers.forEach((answer) => {
     if (answer.isCorrect) correctAnswers.push(answer);
     else incorrectAnswers.push(answer);
-    //
-    const correctAnswerCount = question.expectedNumAnswers;
-    const incorrectAnswerCount = MAX_NUMBER_OF_ANSWERS - correctAnswerCount;
-
-    const rndCorrectIdx = pickUniqueRandomNumbers(
-      correctAnswerCount,
-      0,
-      correctAnswers.length
-    );
-
-    const rndIncorrectIdx = pickUniqueRandomNumbers(
-      incorrectAnswerCount,
-      0,
-      incorrectAnswers.length
-    );
-
-    rndCorrectIdx.forEach((idx) =>
-      answersToRandomize.push(correctAnswers[idx])
-    );
-
-    rndIncorrectIdx.forEach((idx) =>
-      answersToRandomize.push(incorrectAnswers[idx])
-    );
   });
+
+  const correctAnswerCount = question.expectedNumAnswers;
+  const incorrectAnswerCount = Math.min(
+    MAX_NUMBER_OF_ANSWERS - correctAnswerCount,
+    incorrectAnswers.length
+  );
+
+  // Randomize correct and incorrect answers
+  const selectedCorrectAnswers = randomizeArray(correctAnswers).slice(
+    0,
+    correctAnswerCount
+  );
+  const selectedIncorrectAnswers = randomizeArray(incorrectAnswers).slice(
+    0,
+    incorrectAnswerCount
+  );
+
+  // Combine and shuffle the final answers
+  const answersToRandomize = [
+    ...selectedCorrectAnswers,
+    ...selectedIncorrectAnswers,
+  ];
 
   return { ...question, answers: randomizeArray(answersToRandomize) };
 };
